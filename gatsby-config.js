96ee2e24c0fbdf92c58efdafc9eae8c329122066
Nicolas Chaulet <nicolas.chaulet@breather.com>
2018-11-21 11:42:53 -0500

    Fix rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Blog Nicolas Chaulet',
+    description: 'Personal blog',
+    siteUrl: `https://blog.nchaulet.fr`
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -18,6 +20,59 @@ module.exports = {
       options: {
         trackingId: "UA-34324075-3"
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                  limit: 1000
+                ) {
+                  edges {
+                    node {
+                      html
+                      excerpt
+                      frontmatter {
+                        path
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Gatsby RSS Feed",
+          },
+        ],
+      },
+    },
   ],
 }
