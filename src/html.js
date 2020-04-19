fd786dd59db3db24738a2e1dc687c0335ac3c4fb
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2020-04-19 16:05:17 -0400

    Add preconnect headers

--- src/html.js
+++ src/html.js
@@ -11,6 +11,8 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
